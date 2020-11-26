52ea7da976c71e869ebe0789f19be45dc32df14e
Grant Robert Thompson <rvanna@hotmail.co.uk>
2020-11-26 04:39:23 +0000

    everything

--- main.py
+++ main.py
@@ -1,45 +1,88 @@
-import random
-from art import logo,vs
 from game_data import data
+import random
+from art import logo, vs
+from replit import clear
 
-# #'name': 'Instagram',
-#         'follower_count': 346,
-#         'description': 'Social media platform',
-#         'country': 'United States'
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
 
-#print logo
-print(logo)
-print("Who has the most instagram followers?")
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong."""
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
 
 
-compare_A = random.choice(data)
-compare_B = random.choice(data)
+def game():
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
 
-name_A = compare_A["name"]
-name_B = compare_B["name"]
-description_A = compare_A["description"]
-description_B = compare_B["description"]
-country_A = compare_A["country"]
-country_B = compare_B["country"]
-A_followers = int(compare_A["follower_count"])
-B_followers = int(compare_B["follower_count"])
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+    if account_a == account_b:
+      account_b = get_random_account()
 
-print(f"Compare A: {name_A}, a {description_A}, from {country_A}")
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-print(vs)
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
 
-print(f"Compare B: {name_B}, a {description_B}, from {country_B}")
+game()
 
-user_choice = input("Who has more followers? Type 'A' or 'B': ")
-if user_choice.lower() == "a":
-  user_choice = True
-else:
-  user_choice = False
 
-def compare():
-  if A_followers > B_followers:
-    print("well done, correct!")
-    A_followers = B_followers
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
+
+
+    
 
   
   
